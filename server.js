6b7dca3ddd95bbadabbb75602ba8de00598eb143
Vallipuram Selvanesan <vselvane@my.centennialcollege.ca>
2018-08-12 07:00:32 -0400

    seconed edit all contents

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  */
 
 var app = require('./app');
-var debug = require('debug')('comp125-s2018-lesson13:server');
+var debug = require('debug')('comp125-s2018-assignment4:server');
 var http = require('http');
 
 /**
